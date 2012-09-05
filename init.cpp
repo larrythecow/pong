da5034a25ac491cc31881c81830581bfba8b83cb
larrythecow <sid@projekt-turm.de>
2012-09-06 01:12:34 +0200

    logic.cpp: ball drawing test routine in

--- init.cpp
+++ init.cpp
@@ -3,21 +3,20 @@
 bool pong::init(int w, int h, int color) {
     {
         if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-            fprintf(stderr, "unable to initialize SDL %s\n", SDL_GetError());
+            fprintf(stderr, "SDL_Init: %s\n", SDL_GetError());
             exit(-1);
         }
         //SDL_SWSURFACE
         if ((display = SDL_SetVideoMode(w, h, color, SDL_DOUBLEBUF)) < 0) {
-            fprintf(stderr, "unable to set Video Mode %s\n", SDL_GetError());
+            fprintf(stderr, "SDL_SetVideoMode: %s\n", SDL_GetError());
             exit(-1);
         }
 
         int flags = IMG_INIT_JPG | IMG_INIT_PNG;
         int initted = IMG_Init(flags);
         if ((initted & flags) != flags) {
-            printf("IMG_Init: Failed to init required jpg and png support!\n");
-            printf("IMG_Init: %s\n", IMG_GetError());
-            // handle error
+            fprintf(stderr, "IMG_Init: %s\n", IMG_GetError());
+            exit(-1);
         }
     }
     return true;
--- logic.cpp
+++ logic.cpp
@@ -1,13 +1,15 @@
 #include "pong.h"
-int i=0;;
+int i = 0;
+;
+
 void pong::logic() {
 
-        if (i < 20) {
-            printf("pos: x:%d y:%d", i*10, i*10);
-            ball.setX(i*10);
-            ball.setY(i*10);
+    if (i < 20) {
+        printf("pos: x:%d y:%d", i * 10, i * 10);
+        ball.setX(i * 10);
+        ball.setY(i * 10);
 
-            SDL_Delay(100);
-            i++;
-        }
+        SDL_Delay(100);
+        i++;
+    }
 }
