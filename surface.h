8362e4d22029d7d5f663b03d144adb4b593cf096
Jan Müller <jan.mueller.online@googlemail.com>
2012-09-05 23:00:03 +0200

    surface.h created

---
+++ surface.h
@@ -0,0 +1,21 @@
+#ifndef SURFACE_H_
+#define SURFACE_H_ 
+
+class surface
+{
+public:
+	static SDL_Surface* loadImg(const char* path){
+
+
+	}
+	static void drawImg(SDL_Surface src, SDL_Surface dst, int x, int y){
+
+		
+	
+	}
+		
+
+}
+
+
+#endif
