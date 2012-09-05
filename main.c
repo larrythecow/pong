15699735457df4c3d8d19aedc599e64379d72eda
Jan Müller <jan@think420.(none)>
2012-09-05 16:03:00 +0200

    Readme added

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main(int argc, char** argv) {
             switch (event.type) {
                 case SDL_KEYDOWN:
                 {
-                    printf("Press: %s\n", SDL_GetKeyName(event.key.keysym.sym), );
+                    printf("Press: %s\n", SDL_GetKeyName(event.key.keysym.sym));
                     break;
                 }
                 case SDL_KEYUP:
