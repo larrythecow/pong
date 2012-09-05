a471682ea26f87e174bcfbde7f4894d5cf4de11d
larrythecow <sid@projekt-turm.de>
2012-09-05 21:42:15 +0200

    codeifix

--- logic.cpp
+++ logic.cpp
@@ -1,33 +1,6 @@
 #include "pong.h"
 
 void pong::logic() {
-//    SDL_Event event;
-//    int quit = 0;
-//
-//    while (quit == 0) {
-//        while (SDL_PollEvent(&event)) {
-//            switch (event.type) {
-//                case SDL_KEYDOWN:
-//                {
-//                    printf("Press: %s\n", SDL_GetKeyName(event.key.keysym.sym));
-//                    break;
-//                }
-//                case SDL_KEYUP:
-//                {
-//                    printf("Reles: %s\n", SDL_GetKeyName(event.key.keysym.sym));
-//                    break;
-//                }
-//                case SDL_QUIT:
-//                {
-//                    quit = 1;
-//                    break;
-//                }
-//                default:
-//                {
-//                    break;
-//                }
-//            }
-//        }
-//    }
+
 
 }
