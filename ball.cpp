f6a4b08a443a29f8f79136b9f3e4190f59699393
Jan Müller <jan@think420.(none)>
2012-09-05 18:01:27 +0200

    class created: ball

--- ball.cpp
+++ ball.cpp
@@ -3,3 +3,44 @@
 ball::ball()
 {
 }
+
+ball::~ball()
+{
+}
+
+int ball::getX()
+{
+}
+
+void ball::setX(int x)
+{
+    this->x=x;
+}
+
+int ball::getY()
+{
+}
+
+void ball::setY(int y)
+{
+    this->y=y;
+}
+
+int ball::getMoveX()
+{
+}
+
+void ball::setMoveX(int movex)
+{
+    this->movex=movex;
+}
+
+int ball::getMoveY()
+{
+}
+
+void ball::setMoveY(int movey)
+{
+    this->movey=movey;
+}
+
