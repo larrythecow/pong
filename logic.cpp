6665ccc2ad66832f149931b37e7aba3cdc191a7f
larrythecow <sid@projekt-turm.de>
2012-09-06 03:09:22 +0200

    blub

--- logic.cpp
+++ logic.cpp
@@ -4,47 +4,59 @@ void pong::logic() {
     int tmpX = ball.getX();
     int tmpY = ball.getY();
 
-    if (tmpX <= 0 || tmpX >= 800 || tmpY <= 0 || tmpY >= 600 - 10) {
 
-        if (tmpX <= 0) {
-            fprintf(stdout, "Player links Hat gewonnen\n");
-        }
-        if (tmpX >= 790) {
-            fprintf(stdout, "Player links Hat gewonnen");
-        }
+    //            (tmpX + ball.getMoveX) <= (lPad->getX() + lPad->getWidth())
+    //            (tmpX + ball.getMoveX) <= (lPad->getX() + lPad->getWidth()
+    //            ) {
+
+    if (tmpX <= 0) {
+        fprintf(stdout, "Player rechts hat gewonnen\n");
+    } else if (tmpX >= 790) {
+        fprintf(stdout, "Player links hat gewonnen\n");
+    } else
         if (tmpY <= (0 - ball.getMoveY())) {
-            ball.setMoveY(ball.getMoveY()*-1);
-            ball.setX(0);
-        }
+        ball.setMoveY(ball.getMoveY()*-1);
+        ball.setX(0);
+    } else
         if (tmpY >= (600 - ball.getMoveY())) {
-            ball.setMoveY(ball.getMoveY()*-1);
-            ball.setX(600);
-        }
+        ball.setMoveY(ball.getMoveY()*-1);
+        ball.setX(600);
     }
+
+    if ((tmpX + ball.getMoveX) <= (lPad->getX() + lPad->getWidth())) {
+        //do
+    } else if ((tmpX + ball.getMoveX) >= (rPad->getX())) {
+        //do
+    }
+
     ball.setX(tmpX + ball.getMoveX());
     ball.setY(tmpY + ball.getMoveY());
 
-	if (lPad->getState()==PAD_UP){
-		lPad->setY(lPad->getY()-lPad->getSpeed());	
-		if(lPad->getY()<= 0)
-			lPad->setY(0);
-	}
-	else if (lPad->getState()==PAD_DOWN){
-		lPad->setY(lPad->getY()+lPad->getSpeed());	
-		if(lPad->getY()>= 550)
-			lPad->setY(550);
-	}
 
-	if (rPad->getState()==PAD_UP){
-		rPad->setY(rPad->getY()-rPad->getSpeed());	
-		if(rPad->getY()<= 0)
-			rPad->setY(0);
-	}
-	else if (rPad->getState()==PAD_DOWN){
-		rPad->setY(rPad->getY()+rPad->getSpeed());
-		if(rPad->getY()>= 550)
-			rPad->setY(550);
-	}
 
-	SDL_Delay(10);
+
+
+
+
+    if (lPad->getState() == PAD_UP) {
+        lPad->setY(lPad->getY() - lPad->getSpeed());
+        if (lPad->getY() <= 0)
+            lPad->setY(0);
+    } else if (lPad->getState() == PAD_DOWN) {
+        lPad->setY(lPad->getY() + lPad->getSpeed());
+        if (lPad->getY() >= 550)
+            lPad->setY(550);
+    }
+
+    if (rPad->getState() == PAD_UP) {
+        rPad->setY(rPad->getY() - rPad->getSpeed());
+        if (rPad->getY() <= 0)
+            rPad->setY(0);
+    } else if (rPad->getState() == PAD_DOWN) {
+        rPad->setY(rPad->getY() + rPad->getSpeed());
+        if (rPad->getY() >= 550)
+            rPad->setY(550);
+    }
+
+    SDL_Delay(5);
 }
